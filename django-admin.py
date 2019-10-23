d62906fd1a8e4efba35b40a6ac82cee1bb7c1437
Sheril <sjacob2006@gmail.com>
2019-10-23 18:57:08 +0530

    My first django app, first commit

---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\h375447\pycharmprojects\django_application\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
